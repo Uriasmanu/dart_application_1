ea553cabd02fa6136a19176e9167901ba11995d4
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2023-09-10 21:31:48 -0300

    Exercicio 1

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,3 +1,16 @@
 void main() {
-  print('Hello world: ');
+  
+  String fruta = "laranja";
+  double peso = 15.5;
+  bool isMadura;
+  String cor = "verde";
+  int diasDeColida = 40;
+
+  if (diasDeColida >= 30){
+    isMadura = true;
+  }else{
+    isMadura = false;
+  }
+
+  print(isMadura);
 }
